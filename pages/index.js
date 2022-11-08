46dff2e0ee197a76c95473b975795ddfa77f01fc
tojuanin <polvojuan6@gmail.com>
2022-11-08 13:39:16 -0600

    v1

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="unidevsidad" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Bienvenido
         </p>
       </main>
 
