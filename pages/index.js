5c61baec1800904d6672b16eda3291222b93ce9b
tojuanin <polvojuan6@gmail.com>
2022-11-08 14:01:05 -0600

    v3

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Diseño y programación web</title>
+        <title>Unidevsidad</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
